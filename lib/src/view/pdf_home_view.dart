75551b0c0f78c5d932d9df89a80791d649b433f8
cristianoDev <cristianolourenco319@gmail.com>
2025-06-07 13:32:22 +0100

    installing packages and reading pdf files

---
+++ lib/src/view/pdf_home_view.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+
+class PdfHomeView extends StatefulWidget {
+  const PdfHomeView({super.key});
+
+  @override
+  State<PdfHomeView> createState() => _PdfHomeViewState();
+}
+
+class _PdfHomeViewState extends State<PdfHomeView> {
+  File? _pdfFile;
+
+  Future<void> _pickPDF() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
+    if (result != null && result.files.single.path != null) {
+      setState(() {
+        _pdfFile = File(result.files.single.path!);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Leitor de PDF")),
+      body:
+          _pdfFile == null
+              ? const Center(child: Text("Selecione um arquivo pdf"))
+              : PDFView(filePath: _pdfFile!.path),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _pickPDF,
+        child: Icon(Icons.upload_file),
+      ),
+    );
+  }
+}
